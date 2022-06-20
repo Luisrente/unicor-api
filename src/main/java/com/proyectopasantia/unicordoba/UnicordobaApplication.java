5b21fa9a4a625accfd7767d1f75960dbeecf06a5
Luis_R <luisrentef@gmail.com>
2022-06-20 18:57:37 -0500

    firsth commit

---
+++ src/main/java/com/proyectopasantia/unicordoba/UnicordobaApplication.java
@@ -0,0 +1,13 @@
+package com.proyectopasantia.unicordoba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicordobaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicordobaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectopasantia/unicordoba/UnicordobaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectopasantia.unicordoba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicordobaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
